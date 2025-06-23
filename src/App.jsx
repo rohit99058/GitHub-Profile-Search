7a95230e32edeb5647810919558e077dbad4aeb8
Rohit Kumar <patelrr2552001@gmail.com>
2025-06-23 10:41:27 +0530

    this is first time uploading

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css';
+import GithubSearch from './Components/GithubSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubSearch />
+    </div>
+  );
+}
+
+export default App;
